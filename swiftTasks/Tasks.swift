c26de21587d4155a2e365c580c48ffa998621df5
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-07-07 17:58:45 -0400

    second commit

---
+++ swiftTasks/Tasks.swift
@@ -0,0 +1,20 @@
+//
+//  Tasks.swift
+//  swiftTasks
+//
+//  Created by Jack Borthwick on 7/7/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Tasks: NSManagedObject {
+
+    @NSManaged var taskName: String
+    @NSManaged var dateEntered: NSDate
+    @NSManaged var taskDescription: String
+    @NSManaged var taskDifficulty: NSNumber
+    @NSManaged var dateUpdated: NSDate
+
+}
